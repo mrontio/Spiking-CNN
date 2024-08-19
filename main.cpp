0f8062a6c764da03a7900d4c624e8d6ff6f84f03
mrontio <mrontionov@meetowl.site>
2024-08-19 16:03:55 +0100

    Significantly improve testing code

--- main.cpp
+++ main.cpp
@@ -8,24 +8,32 @@
 using namespace std;
 
 int main() {
-        auto conv_weights = Tensor("./weights/conv-weights.npy");
-        auto torch_output = Tensor("./tensors/pool-torch.npy");
-
         auto data = Tensor("./tensors/sample-data.npy");
-        auto x = Tensor(TensorShape{2,34,34});
-        x.fill(1.0);
+        //auto torch_output = Tensor("./tensors/output-3-AvgPool2d.npy");
+        auto out_conv2d = Tensor("./tensors/output-0-Conv2d.npy");
+        auto out_iaf = Tensor("./tensors/output-1-IAFSqueeze.npy");
+        auto recordings = Tensor("./tensors/recording-1-IAFSqueeze.npy");
 
-        auto conv2d = Convolutional(conv_weights, 1, {1, 1});
-        auto conv_out = conv2d.forward(x);
-
-        auto if_layer = IntegrateFire((*conv_out).shape());
-        auto if_out = if_layer.forward((*conv_out));
-
+        auto conv2d = Convolutional(Tensor("./weights/0-Conv2d.npy"), 1, {1, 1});
+        auto if_layer = IntegrateFire(TensorShape{8, 34, 34});
         auto avgpool = AvgPool(2, 2, 0);
-        auto pool_out = avgpool.forward(*if_out);
 
+        int correct = 0;
+        for (shape batch = 0; batch < data.shape(0); batch++) {
+                auto x = data(TensorShape{batch});
+                auto y = out_conv2d(TensorShape{batch});
 
-        cout << pool_out->precisionEqual(torch_output, 8) << endl;
+                auto conv_out = conv2d.forward(*x);
+                auto if_out = if_layer.forward(*conv_out);
+                //auto pool_out = avgpool.forward(*if_out);
+
+                bool cool = conv_out->precisionEqual(*y, 6);
+                if (cool) {
+                        ++correct;
+                }
+                cout << batch << " " << correct << endl;
+        }
+        cout << "Correct: " << correct << ", Wrong: " << data.shape(0) - correct << endl;
 
         return 0;
 }
